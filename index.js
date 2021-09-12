9c5da835ac555047d65c8a1b2cde123431ae772b
Derek Araujo <derekaraujo19@gmail.com>
2021-09-11 23:45:42 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    // .then(res => res.json())
+    .then(function(res) {
+      return res.json();
+    })
+    // .then(json => renderBooks(json));
+    .then(function(json) {
+      return renderBooks(json);
+    })
+
 }
 
 function renderBooks(books) {
